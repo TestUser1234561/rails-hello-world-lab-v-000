0cfe9e24c07a6364433fc960b17565313c5ede3f
Morgan Fudge <mfudge92@gmail.com>
2018-02-16 14:21:04 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+class StaticController < ApplicationController
+  def hello_world
+  end
+end
+
+
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
